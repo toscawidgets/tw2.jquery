7f69c646b088d02906e4f779513acd15c887403d
Joseph Tate <jtate@dragonstrider.com>
2009-10-01 23:31:46 -0400

    Fix resource dependencies

--- tw2/jquery/widgets.py
+++ tw2/jquery/widgets.py
@@ -1,3 +1,4 @@
+from tw2.core import Widget
 from base import jQueryJSLink, jQueryCSSLink, jQueryPluginJSLink, jQueryPluginCSSLink
 import defaults
 
@@ -5,5 +6,8 @@ jquery_js = jQueryJSLink()
 
 
 #Jcrop
+jcrop_css = jQueryPluginCSSLink(name=defaults._jcrop_name_, version=defaults._jcrop_version_)
 jcrop_js = jQueryPluginJSLink(name=defaults._jcrop_name_, version=defaults._jcrop_version_)
-jcrop_css = jQueryPluginCSSLink(name=defaults._jcrop_name_, version=defaults._jcrop_version_, additional_files = ['Jcrop.gif'])
+
+# The usable piece, I couldn't figure out a better way to do dependencies
+jcrop = jQueryJSLink(resources = [jcrop_js, jcrop_css])
