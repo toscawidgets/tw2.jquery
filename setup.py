32b754976ac1788c69b7a3d1f29afa7e402cb13e
Ralph Bean <ralph.bean@gmail.com>
2011-05-03 11:36:34 -0400

    Version bump for new jquery version.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ _extra_mako = ["Mako >= 0.1.1"]
 
 setup(
     name='tw2.jquery',
-    version='2.0b4',
+    version='2.0b5',
     description=long_description ,
     author='Joseph Tate',
     author_email='jtate@dragonstrider.com',
