010c1cf30b078729caa32e196348d4416d0d23c3
Ralph Bean <ralph.bean@gmail.com>
2011-06-14 16:29:28 -0400

    Tests

---
+++ tests/test_jquery.py
@@ -0,0 +1,28 @@
+import tw2.core as twc
+
+def request_local_tst():
+    global _request_local, _request_id
+#    if _request_id is None:
+#        raise KeyError('must be in a request')
+    if _request_local == None:
+        _request_local = {}
+    try:
+        return _request_local[_request_id]
+    except KeyError:
+        rl_data = {}
+        _request_local[_request_id] = rl_data
+        return rl_data
+
+twc.core.request_local = request_local_tst
+_request_local = {}
+_request_id = 'whatever'
+
+def setup():
+    twc.core.request_local = request_local_tst
+    twc.core.request_local()['middleware'] = twc.make_middleware()
+
+
+def test_jquery():
+    from tw2.jquery import jquery_js
+    the_link = '/resources/tw2.jquery/static/jquery/1.6.0/jquery.js'
+    assert(jquery_js.req().link == the_link)
