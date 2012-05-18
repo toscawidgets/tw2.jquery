90605f1a8af377c6ede852a61c050c02460dfbea
Ralph Bean <rbean@redhat.com>
2012-05-18 09:57:17 -0400

    PEP8 sweep.

--- tw2/jquery/base.py
+++ tw2/jquery/base.py
@@ -4,6 +4,7 @@ from version import JSLinkMixin
 
 import defaults
 
+
 class jQueryLinkMixin(JSLinkMixin):
     name = 'jquery'
     dirname = defaults._dirname_
@@ -13,15 +14,28 @@ class jQueryLinkMixin(JSLinkMixin):
 
     modname = 'tw2.jquery'
 
+
 class jQueryJSLink(JSLink, jQueryLinkMixin):
     pass
+
+
 class jQueryCSSLink(jQueryLinkMixin, CSSLink):
     extension = 'css'
 
+
 class jQueryPluginLinkMixin(jQueryLinkMixin):
-    dirname =   Param('(string) Specify the directory path for the given file, relative to the "static" folder some substitutions are allowed. Default: %s' % defaults._plugin_dirname_, default=defaults._plugin_dirname_)
-    basename =  Param('(string) Specify the basename for the given file. Default: %s' % defaults._plugin_basename_, default=defaults._plugin_basename_)
-    subdir = Param('(string) Specify the subdirectory that this link is found in.  Default: %s' % defaults._plugin_subdir_, default=defaults._plugin_subdir_)
+    dirname = Param('(string) Specify the directory path for the given '
+                    'file, relative to the "static" folder some '
+                    'substitutions are allowed. Default: %s' % \
+                    defaults._plugin_dirname_,
+                    default=defaults._plugin_dirname_)
+
+    basename = Param('(string) Specify the basename for the given file. '
+                     'Default: %s' % defaults._plugin_basename_,
+                     default=defaults._plugin_basename_)
+    subdir = Param('(string) Specify the subdirectory that this link is '
+                   'found in.  Default: %s' % defaults._plugin_subdir_,
+                   default=defaults._plugin_subdir_)
 
     @property
     def substitutions(self):
@@ -29,8 +43,10 @@ class jQueryPluginLinkMixin(jQueryLinkMixin):
         subs.update(dict(subdir=self.subdir))
         return subs
 
+
 class jQueryPluginJSLink(JSLink, jQueryPluginLinkMixin):
     subdir = 'js'
+
 
 class jQueryPluginCSSLink(jQueryPluginLinkMixin, CSSLink):
     subdir = 'css'
--- tw2/jquery/version.py
+++ tw2/jquery/version.py
@@ -1,35 +1,42 @@
-# A simple adapter to quickly replace one version with another, swap debug for production, etc.
+# A simple adapter to quickly replace one version with
+# another, swap debug for production, etc.
 
 # Defaults
-_external_          = False
+_external_ = False
 
-#_variant_           = 'min' # set to '' for debugging
-_variant_           = ''
+#_variant_ = 'min' # set to '' for debugging
+_variant_ = ''
 
 import os
 from tw2.core import Link, core
 from tw2.core.params import Param
 import pkg_resources
 
-class JSLinkError(Exception): pass
 
-# The adapter
+class JSLinkError(Exception):
+    pass
+
 
 class JSLinkMixin(Link):
-    name =      Param('(string) The name of the library to link to')
-    dirname =   Param('(string) Specify the directory path for the given file, relative to the "static" folder.  Some substitutions are allowed (name and version).')
-    basename =  Param('(string) Specify the basename for the given file.')
-    version =   Param('(string) Specify the version of the javascript library to use.')
-    external =  Param('(boolean) True if you would like to grab the file from a CDN instead of locally.  Default: False', default=_external_)
-    url_base =  Param('(string) The base url for fetching the javascript library externally')
-    extension = Param('(string) File extension', default = 'js')
-    additional_files =  Param('(list(string)) An optional list of files that should be registered with the static resource handler.  Default: []', default=[])
+    name = Param('(string) The name of the library to link to')
+    dirname = Param('(string) Specify the directory path for the given file, '
+                    'relative to the "static" folder.  Some substitutions are'
+                    ' allowed (name and version).')
+    basename = Param('(string) Specify the basename for the given file.')
+    version = Param('(string) Specify the version of the javascript library '
+                    'to use.')
+    external = Param('(boolean) True if you would like to grab the file from '
+                     'a CDN instead of locally.  Default: False',
+                     default=_external_)
+    url_base = Param('(string) The base url for fetching the javascript '
+                     'library externally')
+    extension = Param('(string) File extension', default='js')
+    additional_files = Param('(list(string)) An optional list of files that '
+                             'should be registered with the static resource '
+                             'handler.  Default: []', default=[])
 
-    variant =   Param('File variant, e.g., (min for minified), default is %s' % _variant_, default=_variant_)
-
-#    @property
-#    def modname(self):
-#        raise NotImplementedError('You must set the modname property on your derived class')
+    variant = Param('File variant, e.g., (min for minified), '
+                    'default is %s' % _variant_, default=_variant_)
 
     def __init__(self, *args, **kw):
         self._link = None
@@ -40,7 +47,10 @@ class JSLinkMixin(Link):
             modname = self.modname or self.__module__
             rl = core.request_local()
             resources = rl['middleware'].resources
-            resources.register(self.modname, os.path.dirname(self.filename), whole_dir=True)
+            resources.register(
+                self.modname,
+                os.path.dirname(self.filename),
+                whole_dir=True)
         super(JSLinkMixin, self).prepare()
 
     @property
@@ -64,23 +74,32 @@ class JSLinkMixin(Link):
             if self.external:
                 link = self.external_link
             else:
-                link = ('/'+'/'.join((mw.config.res_prefix.strip('/'), self.modname, 'static', self.dirname, self.core_filename)) )
+                link = ('/' + '/'.join((
+                    mw.config.res_prefix.strip('/'),
+                    self.modname,
+                    'static',
+                    self.dirname,
+                    self.core_filename
+                )))
             self._link = link
         return self._link % self.substitutions
 
     def _set_link(self, link):
         self._link = link
 
-    link = property(_get_link, _set_link)#Variable('Direct web link to file. If this is not specified, it is automatically generated, based on :attr:`modname` and :attr:`filename`.', default=property(_get_link, _set_link))
+    link = property(_get_link, _set_link)
 
     def abspath(self, filename):
-        return os.sep.join((pkg_resources.resource_filename(self.modname, ''),  filename))
+        return os.sep.join((
+            pkg_resources.resource_filename(self.modname, ''),
+            filename
+        ))
 
     def try_filename(self, filename):
         abspath = self.abspath(filename)
         if os.path.exists(abspath):
             return filename
-        raise JSLinkError('File does not exist: %s'%abspath)
+        raise JSLinkError('File does not exist: %s' % abspath)
 
     @property
     def substitutions(self):
@@ -93,7 +112,9 @@ class JSLinkMixin(Link):
         basename = basename.replace('/', os.sep)
         basename = basename.replace('\\', os.sep)
 
-        filename = os.sep.join(('static', self.dirname, basename)) % self.substitutions
+        filename = os.sep.join(('static', self.dirname, basename)) % \
+                self.substitutions
+
         #try the default
         return self.try_filename(filename)
 
